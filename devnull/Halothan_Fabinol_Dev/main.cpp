6905a5d5b62efc4f9db871ff5cf43e6503752010
Dreikant <fabi1980@gmx.de>
2012-09-16 17:05:30 +0200

    inital commit
    .

---
+++ devnull/Halothan_Fabinol_Dev/main.cpp
@@ -0,0 +1,39 @@
+#include <windows.h>
+#include <stdlib.h>
+
+#include "Detours.h"
+
+#define _DEBUG_LOG    TRUE
+#include "LogFile.h"
+
+#include "BF3_SDK.h"
+
+DWORD CreateDetours()
+{
+	// init hooks.
+	CLogFile logFile = CLogFile("log.txt",true);
+	logFile.Write("Create Detours...");
+	logFile.Write("Create Detours Finished...");
+	fb::DxRenderer* g_dxRenderer = fb::DxRenderer::Singleton( );
+
+	if( g_dxRenderer == NULL )
+	{
+		logFile.Write("Swapchain not found...");
+	}else
+	{
+		logFile.Write("Swapchain found %X ... ",g_dxRenderer->pSwapChain);
+	}
+
+
+
+
+	return 0;
+}
+
+BOOL WINAPI DllMain(HMODULE hModule, DWORD dwReason, LPVOID lpvReserved)
+{
+    if(dwReason == DLL_PROCESS_ATTACH) { 
+		CreateThread(NULL,0,(LPTHREAD_START_ROUTINE)CreateDetours,0,0,0);
+    } 
+    return TRUE;
+}
