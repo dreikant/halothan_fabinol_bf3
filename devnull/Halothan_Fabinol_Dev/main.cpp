59bce99cd13baa001abd046818fd1c79532e388d
Dreikant <fabi1980@gmx.de>
2012-09-16 20:56:54 +0200

    SwapChain Detour in main.cpp

--- devnull/Halothan_Fabinol_Dev/main.cpp
+++ devnull/Halothan_Fabinol_Dev/main.cpp
@@ -8,6 +8,14 @@
 
 #include "BF3_SDK.h"
 
+typedef HRESULT (WINAPI* tPresent)(LPDIRECT3DSWAPCHAIN9 pSwapChain, const RECT *pSourceRect,const RECT *pDestRect,HWND hDestWindowOverride,const RGNDATA *pDirtyRegion,DWORD dwFlags);
+tPresent oPresent;
+
+HRESULT WINAPI hkPresent(LPDIRECT3DSWAPCHAIN9 pSwapChain, const RECT *pSourceRect,const RECT *pDestRect,HWND hDestWindowOverride,const RGNDATA *pDirtyRegion,DWORD dwFlags)
+{
+	return oPresent(pSwapChain, pSourceRect, pDestRect, hDestWindowOverride, pDirtyRegion,dwFlags);
+}
+
 DWORD CreateDetours()
 {
 	// init hooks.
@@ -19,13 +27,23 @@ DWORD CreateDetours()
 	//insekt du sau
 	if( g_dxRenderer == NULL )
 	{
-		logFile.Write("Swapchain not found...");
+		logFile.Write("Renderer not found...");
 	}else
 	{
 		logFile.Write("Swapchain found %X ... ",g_dxRenderer->pSwapChain);
+
+		// try swapchain detour.
+		if ( VALID( g_dxRenderer->pSwapChain ) )
+		{
+			DWORD* vtable    = ( DWORD* )g_dxRenderer->pSwapChain;
+			vtable            = ( DWORD* )vtable[0];
+			oPresent = ( tPresent )DetourCreate( (PBYTE)vtable[8], (PBYTE)&hkPresent, 7);
+			logFile.Write("Detoured Swapchain Function: %X -> %X ... ", g_dxRenderer->pSwapChain, (DWORD)oPresent);
+		}else
+		{
+			logFile.Write("Detour failed. Swapchain Invalid. %X ... ", g_dxRenderer->pSwapChain);
+		}
 	}
-
-
 
 
 	return 0;
